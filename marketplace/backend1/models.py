76f105bceaeacd45558e12fca45d5b36ec601020
Suleiman Mahmood <suleiman.mahmood@gmail.com>
2024-12-21 18:13:23 +0000

    tracking

--- marketplace/backend1/models.py
+++ marketplace/backend1/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     buyer_latitude = models.DecimalField(max_digits=100, decimal_places=20, null=True, blank=True, default=None)
     buyer_longitude = models.DecimalField(max_digits=100, decimal_places=20, null=True, blank=True, default=None)
     status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('rejected', 'Rejected'), ('delivered', 'Delivered'), ('cancelled', 'Cancelled')], default='pending')
+    deliverer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delivered_products', null=True, blank=True)
     class Meta:
         ordering = ['-created_at']
 
